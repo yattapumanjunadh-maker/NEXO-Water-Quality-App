e366724e0e4146a89306c27d6f467caab7ef4530
yattapumanjunadh-maker <yattapumanjunadh@gmail.com>
2026-09-19 00:35:54 +0530

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "NEXO_WaterQualityApp_Final"
+include(":app")
